bfd0dbf806228c88df19ef716ebbf2af4f5bd5c1
wooseong97 <stk14570@kaist.ac.kr>
2025-04-21 22:24:18 +0900

    Update run.sh

--- sel-update-mtl/run1.sh
+++ sel-update-mtl/run1.sh
@@ -4,3 +4,7 @@ port_num=29500
 CUDA_VISIBLE_DEVICES=$device_num python -m torch.distributed.launch --nproc_per_node=1 --master_port $port_num main.py --config_exp './configs/baseline/nyud.yml' --run_mode train
 # CUDA_VISIBLE_DEVICES=$device_num python -m torch.distributed.launch --nproc_per_node=1 --master_port $port_num main.py --config_exp './configs/baseline/pascal.yml' --run_mode train
 # CUDA_VISIBLE_DEVICES=$device_num python -m torch.distributed.launch --nproc_per_node=1 --master_port $port_num main.py --config_exp './configs/baseline/taskonomy.yml' --run_mode train
+
+# CUDA_VISIBLE_DEVICES=$device_num python -m torch.distributed.launch --nproc_per_node=1 --master_port $port_num main.py --config_exp './configs/sel/nyud.yml' --run_mode train
+# CUDA_VISIBLE_DEVICES=$device_num python -m torch.distributed.launch --nproc_per_node=1 --master_port $port_num main.py --config_exp './configs/sel/pascal.yml' --run_mode train
+# CUDA_VISIBLE_DEVICES=$device_num python -m torch.distributed.launch --nproc_per_node=1 --master_port $port_num main.py --config_exp './configs/sel/taskonomy.yml' --run_mode train
